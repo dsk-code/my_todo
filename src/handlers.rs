93f1bcbb1c152039de107b91ae6c0aeb27ee63f7
dsk-code <dsk0425lucky@iCloud.com>
2024-03-04 16:54:24 +0900

    error[E0277]: the trait bound  is not satisfied

--- src/handlers.rs
+++ src/handlers.rs
@@ -1,16 +1,45 @@
 use axum::{
-    extract::{Extension, Path}, 
+    async_trait,
+    extract::{Extension, FromRequest, Path, Request}, 
     http::StatusCode, 
-    response::IntoResponse, 
+    response::IntoResponse,
+    BoxError,
     Json
 };
+use serde::de::DeserializeOwned;
 use std::sync::Arc;
-
+use validator::Validate;
 use crate::repositories::{CreateTodo, TodoRepository, UpdateTodo};
+
+#[derive(Debug)]
+pub struct  ValidatedJson<T>(T);
+
+#[async_trait]
+impl<T, B> FromRequest<B> for ValidatedJson<T>
+where
+    T: DeserializeOwned + Validate,
+    B: http_body::Body + Send + Sync,
+    B::Data: Send,
+    B::Error: Into<BoxError>,
+{
+    type Rejection = (StatusCode, String);
+
+    async fn from_request(req: Request, state: &B) -> Result<Self, Self::Rejection> {
+        let Json(value) = Json::<T>::from_request(req, state).await.map_err(|rejection| {
+            let message = format!("Json parse error: [{}]", rejection);
+            (StatusCode::BAD_REQUEST, message)
+        })?;
+        value.validate().map_err(|rejection| {
+            let message = format!("Validation error: [{}]", rejection).replace('\n', ", ");
+            (StatusCode::BAD_REQUEST, message)
+        })?;
+        Ok(ValidatedJson(value))
+    }
+}
 
 pub async fn create_todo<T: TodoRepository>(
     Extension(repository): axum::Extension<Arc<T>>,
-    Json(payload): Json<CreateTodo>,
+    ValidatedJson(payload): ValidatedJson<CreateTodo>,
 ) -> impl IntoResponse {
     let todo = repository.create(payload);
     (StatusCode::CREATED, Json(todo))
@@ -34,7 +63,7 @@ pub async fn all_todo<T: TodoRepository>(
 pub async fn update_todo<T: TodoRepository>(
     Path(id): Path<i32>,
     Extension(repository): Extension<Arc<T>>,
-    Json(payload): Json<UpdateTodo>,
+    ValidatedJson(payload): ValidatedJson<UpdateTodo>,
 ) -> Result<impl IntoResponse, StatusCode> {
     let todo = repository.update(id, payload).or(Err(StatusCode::NOT_FOUND))?;
     Ok((StatusCode::CREATED, Json(todo)))
--- src/repositories.rs
+++ src/repositories.rs
@@ -6,6 +6,7 @@ use std::{
 use serde::{Deserialize, Serialize};
 use thiserror::Error;
 use anyhow::Context;
+use validator::Validate;
 
 #[derive(Debug, Error)]
 enum RepositoryError {
@@ -28,13 +29,17 @@ pub struct Todo {
     completed: bool,
 }
 
-#[derive(Debug, Serialize, Deserialize, Clone, PartialEq, Eq)]
+#[derive(Debug, Serialize, Deserialize, Clone, PartialEq, Eq, Validate)]
 pub struct CreateTodo {
+    #[validate(length(min = 1, message = "Can not be empty"))]
+    #[validate(length(max = 100, message = "Over text length"))]
     text: String,
 }
 
-#[derive(Debug, Serialize, Deserialize, Clone, PartialEq, Eq)]
+#[derive(Debug, Serialize, Deserialize, Clone, PartialEq, Eq, Validate)]
 pub struct UpdateTodo {
+    #[validate(length(min = 1, message = "Can not be empty"))]
+    #[validate(length(max = 100, message = "Over text length"))]
     text: Option<String>,
     completed: Option<bool>,
 }
